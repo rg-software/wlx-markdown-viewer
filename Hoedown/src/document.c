efd29752ae616e9378303d3bec3c961dab400563
Maxim Mozgovoy <mozgovoy@u-aizu.ac.jp>
2024-01-02 12:16:01 +0900

    require two dashes or equality signs to be recognized as a heading

--- Hoedown/src/document.c
+++ Hoedown/src/document.c
@@ -1520,13 +1520,13 @@ is_headerline(uint8_t *data, size_t size)
 	size_t i = 0;
 
 	/* test of level 1 header */
-	if (data[i] == '=') {
+	if (size >= 2 && data[i] == '=') {
 		for (i = 1; i < size && data[i] == '='; i++);
 		while (i < size && data[i] == ' ') i++;
 		return (i >= size || data[i] == '\n') ? 1 : 0; }
 
 	/* test of level 2 header */
-	if (data[i] == '-') {
+	if (size >= 2 && data[i] == '-') {
 		for (i = 1; i < size && data[i] == '-'; i++);
 		while (i < size && data[i] == ' ') i++;
 		return (i >= size || data[i] == '\n') ? 2 : 0; }
